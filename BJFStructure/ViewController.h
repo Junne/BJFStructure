3dd3d2a98b952bc95dabc53675415434b09e4b2f
Junne <junne.bai@gmail.com>
2015-04-16 14:13:46 +0800

    Initial Commit

---
+++ BJFStructure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BJFStructure
+//
+//  Created by baijf on 4/16/15.
+//  Copyright (c) 2015 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BJFStructure/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BJFStructure
+//
+//  Created by baijf on 4/16/15.
+//  Copyright (c) 2015 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
